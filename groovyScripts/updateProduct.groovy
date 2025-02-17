911f915a7422f42d9d206fa4f019eab35859a07b
Rakshit Upadhyay <rakshitupadhyay.2004@gmail.com>
2025-02-17 14:16:31 +0530

    minor changes in updateProduct.groovy

--- groovyScripts/updateProduct.groovy
+++ groovyScripts/updateProduct.groovy
@@ -1,42 +1,52 @@
 import java.sql.Timestamp
+import org.apache.ofbiz.entity.util.*;
 import org.apache.ofbiz.base.util.UtilDateTime
 import org.apache.ofbiz.entity.GenericValue
 import java.sql.Timestamp
+import org.apache.ofbiz.entity.GenericValue
+import org.apache.ofbiz.entity.Delegator
 
 
-def updateProduct(){
-    Map<String, Object> existingProduct = dispatcher.runSync("findProductService",context)
-    if(existingProduct.productList){
-        logInfo("-----ProductFound---------${existingProduct.productList}")
-    }
-
+def updateProduct() {
+    Timestamp currentTimeStamp = UtilDateTime.nowTimestamp()
     Delegator delegator = (Delegator) context.delegator
-
-    GenericValue productDetails = delegator.findOne("Product", ["productId": context.prodId], false)
-    GenericValue existingPrice = delegator.findByAnd("ProductPrice", ["productId": context.prodId], null, false)?.get(0)
-
-    if (productDetails && existingPrice) {
-        if (context.prodName) {
-            productDetails.set("productName", context.prodName)
-        }
-
-        delegator.store(productDetails)
-        Timestamp currentTimestamp = UtilDateTime.nowTimestamp()
-        existingPrice.set("thruDate", currentTimestamp)
-        delegator.store(existingPrice)
-
-        if (context.prodPrice) {
-            GenericValue newPriceEntry = delegator.makeValue("ProductPrice")
-
-            newPriceEntry.set("productId", context.prodId)
-            newPriceEntry.set("productPriceTypeId", "DEFAULT_PRICE")
-            newPriceEntry.set("productPricePurposeId", "PURCHASE")
-            newPriceEntry.set("currencyUomId", "USD")
-            newPriceEntry.set("productStoreGroupId", "_NA_")
-            newPriceEntry.set("price", context.prodPrice)
-            newPriceEntry.set("fromDate", currentTimestamp)
-            delegator.create(newPriceEntry)
-        }
+    logInfo("---Update Service Called---")
+    GenericValue productDetails = delegator.findOne("Product", ["productId": context.productId], false)
+    if (!productDetails) {
+        logInfo("Product not found for ID: ${context.productId}")
+        return error("Product not found")
     }
+    logInfo("---Existing Product Details---${productDetails}----")
+    List<GenericValue> existingPriceList = from("ProductPrice").where("productId", context.productId).queryList()
+    GenericValue productPrice = EntityUtil.getFirst(EntityUtil.filterByDate(existingPriceList))
+    logInfo("---Existing Valid Product Price Details---${productPrice}----")
+
+    if (context.productName) {
+        logInfo("---ProdunctName changed from ${productDetails.productName} to ${context.productName}----")
+        productDetails.set("productName", context.productName)
+        delegator.store(productDetails)
+    }
+
+    if (productPrice) {
+        productPrice.set("thruDate", currentTimeStamp)
+        delegator.store(productPrice)
+    }
+    Map result = success()
+
+    if (context.price) {
+        GenericValue newProductPrice = delegator.makeValue("ProductPrice", [
+                "productId"             : context.productId,
+                "productPriceTypeId"    : "DEFAULT_PRICE",
+                "productPricePurposeId" : "PURCHASE",
+                "currencyUomId"         : "USD",
+                "productStoreGroupId"   : "_NA_",
+                "price"                 : new BigDecimal(context.price ?: 0),
+                "fromDate"              : currentTimeStamp
+        ])
+        result.price = newProductPrice.price
+        logInfo("---New Product Price Record Created ${productPrice}----")
+        delegator.create(newProductPrice)
+    }
+    result.productId = productDetails.productId
 }
 
